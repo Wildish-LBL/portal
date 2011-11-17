600a025d4de4443acd0de39e4ac3da0a59cda251
Piotr Hołubowicz <piotrhol@man.poznan.pl>
2011-11-17 10:57:01 +0100

    Initial commit.

---
+++ src/main/java/pl/psnc/dl/wf4ever/portal/WicketApplication.java
@@ -0,0 +1,31 @@
+package pl.psnc.dl.wf4ever.portal;
+
+import org.apache.wicket.protocol.http.WebApplication;
+
+/**
+ * Application object for your web application. If you want to run this application without deploying, run the Start class.
+ * 
+ * @see pl.psnc.dl.wf4ever.portal.Start#main(String[])
+ */
+public class WicketApplication extends WebApplication
+{    	
+	/**
+	 * @see org.apache.wicket.Application#getHomePage()
+	 */
+	@Override
+	public Class<HomePage> getHomePage()
+	{
+		return HomePage.class;
+	}
+
+	/**
+	 * @see org.apache.wicket.Application#init()
+	 */
+	@Override
+	public void init()
+	{
+		super.init();
+
+		// add your configuration here
+	}
+}
