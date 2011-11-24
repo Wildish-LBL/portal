393f9e95e5b8a15951444b76c1a4207ac1310ad1
Piotr Hołubowicz <piotrhol@man.poznan.pl>
2011-11-24 10:11:04 +0100

    Fixed some links.

--- src/main/java/pl/psnc/dl/wf4ever/portal/TemplatePage.java
+++ src/main/java/pl/psnc/dl/wf4ever/portal/TemplatePage.java
@@ -1,6 +1,7 @@
 package pl.psnc.dl.wf4ever.portal;
 
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class TemplatePage
@@ -12,5 +13,7 @@ public class TemplatePage
 
 	public TemplatePage(final PageParameters parameters)
 	{
+		add(new BookmarkablePageLink<Void>("menu-home", HomePage.class));
+		add(new BookmarkablePageLink<Void>("menu-myros", MyRosPage.class));
 	}
 }
