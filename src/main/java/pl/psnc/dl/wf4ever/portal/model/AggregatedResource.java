59574e9079b57e026e90a0c64994dbaae44a1971
Piotr Hołubowicz <piotrhol@man.poznan.pl>
2011-12-20 09:49:47 +0100

    Aggregated resources as a TreeModel

---
+++ src/main/java/pl/psnc/dl/wf4ever/portal/model/AggregatedResource.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package pl.psnc.dl.wf4ever.portal.model;
+
+import java.net.URI;
+
+/**
+ * @author piotrhol
+ *
+ */
+public class AggregatedResource
+{
+
+	private final URI uri;
+
+	private final String name;
+
+
+	/**
+	 * @param uri
+	 */
+	public AggregatedResource(URI uri, String name)
+	{
+		this.uri = uri;
+		this.name = name;
+	}
+
+
+	/**
+	 * @return the uri
+	 */
+	public URI getUri()
+	{
+		return uri;
+	}
+
+
+	/**
+	 * @return the name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+}
