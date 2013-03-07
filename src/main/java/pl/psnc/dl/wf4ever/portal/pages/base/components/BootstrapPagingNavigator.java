ac4aadf38213d5ae1e5b40b91133b8bdc6aa6a1e
Piotr Hołubowicz <piotrhol@man.poznan.pl>
2013-03-07 18:04:10 +0100

    Pagination works and looks good. Now the results must be fetched on demand, not all at once.

--- src/main/java/pl/psnc/dl/wf4ever/portal/pages/base/components/BootstrapPagingNavigator.java
+++ src/main/java/pl/psnc/dl/wf4ever/portal/pages/base/components/BootstrapPagingNavigator.java
@@ -1,6 +1,8 @@
 package pl.psnc.dl.wf4ever.portal.pages.base.components;
 
+import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.link.AbstractLink;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.navigation.paging.IPageable;
 import org.apache.wicket.markup.html.navigation.paging.IPagingLabelProvider;
 import org.apache.wicket.markup.html.navigation.paging.PagingNavigation;
@@ -19,19 +21,45 @@ public class BootstrapPagingNavigator extends PagingNavigator {
 
     @Override
     protected AbstractLink newPagingNavigationLink(String id, IPageable pageable, int pageNumber) {
-        return new BootstrapPagingNavigationLink(id, pageable, pageNumber);
+        return new Wrapper(id, new BootstrapPagingNavigationLink(id + "Inside", pageable, pageNumber));
     }
 
 
     @Override
     protected AbstractLink newPagingNavigationIncrementLink(String id, IPageable pageable, int increment) {
-        return new BootstrapPagingNavigationIncrementLink(id, pageable, increment);
+        return new Wrapper(id, new BootstrapPagingNavigationIncrementLink(id + "Inside", pageable, increment));
     }
 
 
     @Override
     protected PagingNavigation newNavigation(String id, IPageable pageable, IPagingLabelProvider labelProvider) {
         return new BootstrapPagingNavigation(id, pageable, labelProvider);
+    }
+
+
+    class Wrapper extends AbstractLink {
+
+        /** id. */
+        private static final long serialVersionUID = -527364568167079132L;
+
+        private Link<Void> link;
+
+
+        public Wrapper(String id, Link<Void> link) {
+            super(id);
+            this.link = link;
+            add(link);
+        }
+
+
+        @Override
+        protected void onComponentTag(ComponentTag tag) {
+            super.onComponentTag(tag);
+            if (!link.isEnabled()) {
+                tag.put("class", "disabled");
+            }
+        }
+
     }
 
 }
